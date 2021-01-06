19a23e981d6f003867ca192ac7d5552427caf92a
Darin99 <43581232+Darin99@users.noreply.github.com>
2021-01-06 18:30:51 +0200

    Add files via upload

---
+++ src/Game.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+
+public class Game {
+
+    private GameLogic logic;
+
+    public Game() {
+        this.setLogic(new GameLogic());
+    }
+
+    public GameLogic getLogic() {
+        return logic;
+    }
+
+    public void setLogic(GameLogic logic) {
+        this.logic = logic;
+    }
+
+    public void init() {
+        System.out.println("Enter the Difficulty Level");
+        System.out.println("Press 0 for BEGINNER (9 * 9 Cells and 10 Mines)");
+        System.out.println("Press 1 for INTERMEDIATE (16 * 16 Cells and 40 Mines)");
+        System.out.println("Press 2 for ADVANCED (24 * 24 Cells and 99 Mines)");
+    }
+
+    public void start() throws IOException {
+        this.getLogic().loadField();
+        this.getLogic().play();
+    }
+}
---
+++ src/ReadData.java
@@ -0,0 +1,6 @@
+import java.io.IOException;
+
+public interface ReadData {
+
+    String readData() throws IOException;
+}
